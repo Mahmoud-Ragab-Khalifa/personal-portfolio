1dd57893320b20198b9995c9893eb8413edc89b9
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-03 01:26:11 +0300

    feat: finalize desktop view in navbar

--- src/app/layout/Header/index.tsx
+++ src/app/layout/Header/index.tsx
@@ -1,4 +1,6 @@
+import { navLinks } from "@/constants/navLinks";
 import Logo from "../components/Logo";
+import Button from "@/app/components/Button";
 
 const Header = () => {
   return (
@@ -6,6 +8,24 @@ const Header = () => {
       <nav className="container flex items-center justify-between">
         {/* Personal Logo */}
         <Logo />
+
+        {/* Desktop Nav */}
+        <div className="hidden md:flex items-center gap-1 glass rounded-full px-2 py-1">
+          {navLinks.map((link, index) => (
+            <a
+              href={link.href}
+              key={index}
+              className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface transition-all duration-300"
+            >
+              {link.label}
+            </a>
+          ))}
+        </div>
+
+        {/* CTA Contact Me Button In Desktop View*/}
+        <Button size="sm" className="hidden md:flex">
+          Contact Me
+        </Button>
       </nav>
     </header>
   );
