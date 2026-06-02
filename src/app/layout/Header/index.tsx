b5ecd15ad0136fbdb8967b397c26420b5ae5cb20
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-03 01:15:20 +0300

    feat: add logo

--- src/app/layout/Header/index.tsx
+++ src/app/layout/Header/index.tsx
@@ -1,7 +1,12 @@
+import Logo from "../components/Logo";
+
 const Header = () => {
   return (
     <header className="fixed inset-x-0 inset-s-0 py-5">
-      <nav className="container flex items-center justify-between">Header</nav>
+      <nav className="container flex items-center justify-between">
+        {/* Personal Logo */}
+        <Logo />
+      </nav>
     </header>
   );
 };
---
+++ src/app/layout/components/Logo.tsx
@@ -0,0 +1,12 @@
+const Logo = () => {
+  return (
+    <a
+      href="#"
+      className="text-xl font-bold tracking-tight hover:text-primary transition-colors duration-300"
+    >
+      Mahmoud<span className="text-primary">.</span>
+    </a>
+  );
+};
+
+export default Logo;
