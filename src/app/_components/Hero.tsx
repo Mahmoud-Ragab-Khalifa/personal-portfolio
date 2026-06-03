92141da0d3cd27dc75707d9bde1bdf564e586679
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-03 06:09:31 +0300

    feat: add overlay on the hero section image and add dummy dots moves in hero section

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,3 +1,4 @@
+import { dots } from "@/constants/dots";
 import Image from "next/image";
 
 const Hero = () => {
@@ -13,6 +14,25 @@ const Hero = () => {
           priority={true}
           loading="eager"
         />
+
+        {/* Image Overlay */}
+        <div className="absolute inset-0 bg-linear-to-b from-background/20 via-background/80 to-background" />
+      </div>
+
+      {/* Dummy Dots */}
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        {dots.map((dot, idx) => (
+          <div
+            key={idx}
+            className="absolute size-1.5 rounded-full bg-primary/60"
+            style={{
+              left: dot.left,
+              top: dot.top,
+              animation: `slow-drift ${dot.duration} ease-in-out infinite`,
+              animationDelay: dot.delay,
+            }}
+          />
+        ))}
       </div>
     </section>
   );
