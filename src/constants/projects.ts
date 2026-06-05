a5c5e5b2aadf53f52ceb239c5f630d24ee9ca5f5
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-05 03:18:02 +0300

    feat: add projects array to render projects cards

---
+++ src/constants/projects.ts
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+    title: "Movie Night",
+    description:
+      "Movie discovery web app built with Next.js, TypeScript, and Tailwind CSS. Browse trending movies, search instantly, explore detailed movie pages, and enjoy a modern responsive UI inspired by streaming platforms.",
+    image: "/projects-images/movie-night.PNG",
+    tags: ["Next.js", "Typescript", "Tailwind-CSS", "Supabase"],
+    link: "https://movie-night-hd.vercel.app",
+    github: "https://github.com/Mahmoud-Ragab-Khalifa/movie-night",
+  },
+  {
+    title: "Pizza Station",
+    description:
+      "Production-ready pizza ordering app showcasing modern frontend architecture with Next.js, authentication, cart system, and theming support.",
+    image: "/projects-images/pizza-station.PNG",
+    tags: ["Next-Auth", "Prisma-ORM", "Shadcn-UI", "Redux"],
+    link: "https://the-pizza-station.vercel.app",
+    github: "https://github.com/Mahmoud-Ragab-Khalifa/pizza-station",
+  },
+  {
+    title: "Market",
+    description:
+      "Modern e-commerce application built with Next.js App Router, Tailwind CSS, and shadcn/ui. Features server-side rendering, optimized data fetching, scalable architecture, and clean UI components.",
+    image: "/projects-images/market.PNG",
+    tags: ["Next-Intl", "Zustand", "Firebase", "Next-Themes"],
+    link: "https://the-market-e-commerce.vercel.app",
+    github: "https://github.com/Mahmoud-Ragab-Khalifa/market",
+  },
+  {
+    title: "Homely",
+    description:
+      "Homely is a modern stays platform built with Next.js, Supabase, and Mapbox, offering location-based exploration, interactive maps, and responsive listings.",
+    image: "/projects-images/homely.PNG",
+    tags: ["Next.js", "Server-Actions", "React-Swiper", "Zod"],
+    link: "https://homely-air.vercel.app",
+    github: "https://github.com/Mahmoud-Ragab-Khalifa/homely",
+  },
+  {
+    title: "Social-Media-App",
+    description:
+      "Built a responsive social media platform with user authentication, post creation, editing, and content management features using HTML, CSS, JavaScript, Bootstrap, and Axios.Integrated API communication for login, registration, and real-time post operations while delivering a smooth and user-friendly experience.",
+    image: "/projects-images/social-media-app.PNG",
+    tags: ["Axios", "Bootstrap", "HTML", "JS"],
+    link: "https://mahmoud-ragab-khalifa.github.io/Social-Media-APP",
+    github: "https://github.com/Mahmoud-Ragab-Khalifa/Social-Media-APP",
+  },
+  {
+    title: "Dashboard",
+    description:
+      "Designed and developed a modern dashboard interface using HTML and CSS to practice responsive layouts, component structuring, and UI design principles.Focused on creating a clean, professional user experience with well-organized sections, cards, and navigation elements.",
+    image: "/projects-images/dashboard.PNG",
+    tags: ["HTML", "CSS"],
+    link: "https://mahmoud-ragab-khalifa.github.io/Dashboard",
+    github: "https://github.com/Mahmoud-Ragab-Khalifa/Dashboard",
+  },
+];
