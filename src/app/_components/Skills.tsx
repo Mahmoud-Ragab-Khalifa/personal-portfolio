eb6d4d9a5973bc8835f84ce9a3232a07a27b928b
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-05 06:06:18 +0300

    style: add hover effect on skills cards

--- src/app/_components/Skills.tsx
+++ src/app/_components/Skills.tsx
@@ -15,7 +15,10 @@ const Skills = () => {
       <div className="absolute right-0 inset-y-0 w-32 bg-linear-to-l from-background to-transparent z-10" />
       <Marquee speed={50} gradient={false}>
         {skills.map((skill) => (
-          <div key={skill.name} className="glass p-6 rounded-2xl w-50 mx-3">
+          <div
+            key={skill.name}
+            className="glass p-6 rounded-2xl w-50 mx-3 transition-all duration-300 hover:border hover:border-border select-none"
+          >
             <skill.icon color={skill.color} size={50} className="mx-auto" />
             <span className="block text-center pt-4">{skill.name}</span>
           </div>
